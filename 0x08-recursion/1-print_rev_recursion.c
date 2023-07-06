652f9db4723cb4c1cce8200e45a02d077a5c5142
Joewahongo <joewahongo@gmail.com>
2023-07-06 04:58:35 -0700

    1-print_rev_recursion.c

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - Print string in reverse
+ * @s: String character argument
+ *
+ * Return: Always 0 Success
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	_print_rev_recursion(s + 1);
+	_putchar(*s);
+}
