ceac21a320a3255d377bb2ea723169a9155ed896
Joewahongo <joewahongo@gmail.com>
2023-07-06 15:45:36 -0700

    2-strlen_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,31 +1,14 @@
 #include "main.h"
 
 /**
- * _strlen_recursion - calculate the length of a string
- * @s: the string to count
- *
- * Return: integer value
+ * _strlen_recursion - returns the length of a string.
+ * @s: string
+ * Return: the length of a string.
  */
 int _strlen_recursion(char *s)
 {
 	if (*s == '\0')
-	{
 		return (0);
-	}
-	return (1 + _strlen_recursion(s + 1));
-}
-#include <stdio.h>
-
-/**
- * main - Entry point
- *
- * Return: Always 0 Success
- */
-int main(void)
-{
-	int length;
-
-	length = _strlen_recursion("Corbin Coleman");
-	printf("%d/n", length);
-	return (0);
+	else
+		return (1 + _strlen_recursion(s + 1));
 }
